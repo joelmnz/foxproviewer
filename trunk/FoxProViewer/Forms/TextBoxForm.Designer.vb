c475e909b237319a541b5e1c3baca552f58234af
joel.meikle@gmail.com <joel.meikle@gmail.com@437c222e-9319-11dd-8d1e-f9dfc5e22c32>
2014-04-23 21:43:49 +0000

    Upated Solution to VS2010 Added a Tools -> Connection String : outputs connection string to use and some vb.net code to get you started. (must select a database and table before using this feature).

---
+++ trunk/FoxProViewer/Forms/TextBoxForm.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TextBoxForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(TextBoxForm))
+        Me.BtnClose = New System.Windows.Forms.Button()
+        Me.TxtMessage = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'BtnClose
+        '
+        Me.BtnClose.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnClose.Location = New System.Drawing.Point(750, 306)
+        Me.BtnClose.Name = "BtnClose"
+        Me.BtnClose.Size = New System.Drawing.Size(75, 23)
+        Me.BtnClose.TabIndex = 0
+        Me.BtnClose.Text = "&Close"
+        Me.BtnClose.UseVisualStyleBackColor = True
+        '
+        'TxtMessage
+        '
+        Me.TxtMessage.AcceptsReturn = True
+        Me.TxtMessage.AcceptsTab = True
+        Me.TxtMessage.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TxtMessage.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtMessage.Location = New System.Drawing.Point(12, 12)
+        Me.TxtMessage.Multiline = True
+        Me.TxtMessage.Name = "TxtMessage"
+        Me.TxtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TxtMessage.Size = New System.Drawing.Size(813, 288)
+        Me.TxtMessage.TabIndex = 1
+        Me.TxtMessage.WordWrap = False
+        '
+        'TextBoxForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(837, 341)
+        Me.Controls.Add(Me.TxtMessage)
+        Me.Controls.Add(Me.BtnClose)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "TextBoxForm"
+        Me.Text = "TextBoxForm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents BtnClose As System.Windows.Forms.Button
+    Private WithEvents TxtMessage As System.Windows.Forms.TextBox
+End Class
